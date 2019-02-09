0aaeac108cb89c442bbd06e386a48b69aab2f1d4
hello-HELLO <juliantcchu@gmail.com>
2019-02-09 13:48:41 +0800

    Add files via upload

---
+++ train_model_updated.py
@@ -0,0 +1,48 @@
+from __future__ import print_function
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv3D, MaxPooling3D
+from keras import backend as K
+
+batch_size = 16
+num_classes = 23
+epochs = 100
+
+import pickle
+
+pickle_in = open("X.pickle","rb")
+X = pickle.load(pickle_in)
+X = X.reshape(5520, 100, 100, 39, 1)
+
+pickle_in = open("y.pickle","rb")
+y = pickle.load(pickle_in)
+
+X = X/255.0
+
+
+# convert class vectors to binary class matrices
+y = keras.utils.to_categorical(y, num_classes)
+
+model = Sequential()
+model.add(Conv3D(32, kernel_size=(3, 3, 3),
+                 activation='relu'))
+model.add(Conv3D(64, (3, 3, 3), activation='relu'))
+model.add(MaxPooling3D(pool_size=(2, 2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
+
+model.fit(X, y, batch_size=batch_size, epochs=epochs, validation_split=0.1)
+
+model.save("all_lips.h5")
+
+##score = model.evaluate(x_test, y_test, verbose=0)
+##print('Test loss:', score[0])
+##print('Test accuracy:', score[1])
